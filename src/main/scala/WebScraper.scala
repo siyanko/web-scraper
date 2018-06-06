c910d33adb926d43b598664be077d2a49da10c99
Oleksandr Siianko <oleksandr.siyanko@gmail.com>
2018-06-06 18:20:21 +0200

    add multiple pages processing

--- src/main/scala/WebScraper.scala
+++ src/main/scala/WebScraper.scala
@@ -1,32 +1,72 @@
+
 import cats.effect._
+import cats.implicits._
 import console._
 import htmlPageParser._
 import io.circe.generic.auto._
 import io.circe.syntax._
+import org.http4s.Uri
 import org.http4s.client.blaze._
 import org.http4s.dsl.io._
 
+import scala.util.Either
+
+// NOT FOR PRODUCTION
+import scala.concurrent.ExecutionContext.Implicits.global
+
+// TODO: save data to file
+// TODO: collect product urls
 object WebScraper {
+
+  val pages: List[InverterRequestPage] = List(
+    InverterRequestPage("fronius-galvo/fronius-galvo-1-5-1", "Fronius Galvo 1.5-1", "bacc_45e3b058-7e9d-4f21-a042-edb11b9efdd0_372160b4-7b89-436b-80c5-d56ae52046d5_"),
+    InverterRequestPage("fronius-galvo/fronius-galvo-2-0-1", "Fronius Galvo 2.0-1", "bacc_04cb292b-0b0e-41ff-a44a-b3b4cc786cd9_372160b4-7b89-436b-80c5-d56ae52046d5_"),
+    InverterRequestPage("fronius-galvo/fronius-galvo-2-5-1", "Fronius Galvo 2.5-1", "bacc_62e3e20e-25dd-42ab-9758-8029243cf005_372160b4-7b89-436b-80c5-d56ae52046d5_"),
+    InverterRequestPage("fronius-galvo/fronius-galvo-3-0-1", "Fronius Galvo 3.0-1", "bacc_426411f5-b1bb-4747-9100-291b3fb0fb5d_372160b4-7b89-436b-80c5-d56ae52046d5_"),
+    InverterRequestPage("fronius-galvo/fronius-galvo-3-1-1", "Fronius Galvo 3.1-1", "bacc_105735c3-5168-46d2-81c2-bf4f454157d9_372160b4-7b89-436b-80c5-d56ae52046d5_")
+  )
 
   val program: IO[Unit] = for {
     httpClient <- Http1Client[IO]()
-    _ <- Console[IO].println("Fetching Fronius Galvo 2.0-1 page")
-    page <- httpClient.expect[String]("http://www.fronius.com/en/photovoltaics/products/all-products/inverters/fronius-galvo/fronius-galvo-2-0-1")
-    _ <- Console[IO].println("GOT Fronius Galvo 2.0-1 page")
-    bodyElements = InverterPage.froniusInverterPage.parse(page)
-    _ <- bodyElements match{
-      case Left(err) => for {
+    _ <- Console[IO].println("Fetching Fronius Inverter pages")
+
+    // fetching data from html pages
+    pagesRawData <- fs2.async.parallelTraverse[List, IO, InverterRequestPage, (InverterRequestPage, Either[Throwable, List[InverterRawParameter]])](pages) {
+      p =>
+        val target = Uri.uri("http://www.fronius.com/en/photovoltaics/products/all-products/inverters/") / p.url
+        for {
+          _ <- Console[IO].println("Getting " + p.model)
+          htmlPage <- httpClient.expect[String](target)
+          _ <- Console[IO].println("Got " + p.model)
+        } yield ((p, InverterPage.froniusInverterPage.parse(p.id)(htmlPage)))
+    }
+
+    _ <- Console[IO].println("Got all pages.")
+
+    // converting response to json
+    data <- fs2.async.parallelTraverse[List, IO, (InverterRequestPage, Either[Throwable, List[InverterRawParameter]]), Option[InverterData]](pagesRawData) { tuple =>
+      tuple._2 match {
+        case Left(err) => for {
           _ <- Console[IO].println("Could not parse inverter data, because of " + err.getMessage)
           _ <- IO(err.printStackTrace)
-        } yield ()
-      case Right(v) => 
-        Console[IO].println(v.asJson.toString)
+        } yield None
+        case Right(v) =>
+          val data = InverterData("Fronius", tuple._1.model, v)
+          IO(Some(data))
+      }
     }
-    _ <- Console[IO].println("")
+    _ <- Console[IO].println("Here they are....")
+    _ <- Console[IO].println(data.asJson.toString)
+
     _ <- Console[IO].println("Closing http client")
     _ <- httpClient.shutdown
+
     _ <- Console[IO].println("See you next time, Chao!")
   } yield ()
 
   def main(args: Array[String]): Unit = program.unsafeRunSync()
 }
+
+final case class InverterRequestPage(url: String, model: String, id: String)
+
+final case class InverterData(manufacturer: String, model: String, rawParams: List[InverterRawParameter])
--- src/main/scala/htmlPageParser.scala
+++ src/main/scala/htmlPageParser.scala
@@ -11,25 +11,24 @@ object htmlPageParser {
 
   final case class InverterRawParameter(key: String, value: String)
 
-  trait InverterPage{
-    def parse(p: Page): Either[Throwable, List[InverterRawParameter]]
+  trait InverterPage {
+    def parse(id: String)(p: Page): Either[Throwable, List[InverterRawParameter]]
   }
 
   object InverterPage {
     val froniusInverterPage = new InverterPage {
 
-      private val parseTable:  Element => List[InverterRawParameter] = table => {
+      private val parseTable: Element => List[InverterRawParameter] = table => {
         table.select("tr").asScala.toList
-          .flatMap(tr => tr.children().asScala.toList.map(_.text()).sliding(2).map{
+          .flatMap(tr => tr.children().asScala.toList.map(_.text()).sliding(2).map {
             case List(a, b) => InverterRawParameter(a, b)
           }.toList)
       }
 
-      override def parse(p: Page): Either[Throwable, List[InverterRawParameter]] = Try {
-        val doc = Jsoup.parse(p)
-        val body = doc.body()
-        //    body.getElementById("bacc_45e3b058-7e9d-4f21-a042-edb11b9efdd0_372160b4-7b89-436b-80c5-d56ae52046d5_") // fronius galvo 1.5-1
-        body.getElementById("bacc_04cb292b-0b0e-41ff-a44a-b3b4cc786cd9_372160b4-7b89-436b-80c5-d56ae52046d5_") // fronius galvo 2.0-1
+      // TODO: improve parsing logic
+      override def parse(id: String)(p: Page): Either[Throwable, List[InverterRawParameter]] = Try {
+        Jsoup.parse(p).body()
+          .getElementById(id)
           .getElementsByTag("table")
           .asScala.toList
           .flatMap(parseTable)
